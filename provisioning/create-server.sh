311e8ced3d556eeb09f8aec3b6eb40916ede21f6
John Garbutt <john.garbutt@stackhpc.com>
2018-05-02 11:56:49 +0100

    Fix up provision server script

--- provisioning/create-server.sh
+++ provisioning/create-server.sh
@@ -11,20 +11,22 @@ NAME="spark-ukt0-test"
 FLAVOR=${FLAVOR:-"4x16"}
 IMAGE_NAME=${IMAGE_NAME:-"CentOS7"}
 USER="centos"
-KEYPAIR_NAME=${KEYPAIR_NAME:-usual}
+KEYPAIR_NAME=${KEYPAIR_NAME:-mine}
 NETWORK_NAME=${NETWORK_NAME:-test}
 
 export OS_CLOUD=${OS_CLOUD:-"hphi"}
 
 if ! openstack server show $NAME >/dev/null 2>&1; then
 	openstack server create \
-		--image $IMAGE_NAME
-		--flavor $FLAVOR
-		--key-name $KEYPAIR_NAME
-		--network $NETWORK_NAME
-		--wait
+		--image $IMAGE_NAME \
+		--flavor $FLAVOR \
+		--key-name $KEYPAIR_NAME \
+		--network $NETWORK_NAME \
+		--wait \
 		$NAME
 fi
 
 server_info=$(openstack server show $NAME --format json)
 echo $server_info
+
+deactivate
